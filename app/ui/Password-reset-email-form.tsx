5df13643c177cfb89426ab170073d998a18d2e8a
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-25 18:10:34 +0100

    update

--- app/ui/Password-reset-email-form.tsx
+++ app/ui/Password-reset-email-form.tsx
@@ -60,53 +60,64 @@ export default function ResetPasswordForm() {
     setLoading(false);
   };
   return (
-    <form
-      className="space-y-3"
-      onSubmit={async (e) => {
-        e.preventDefault();
-        await handleSubmit(e);
-      }}
-    >
-      <div className="flex-1 rounded-lg bg-gray-50 dark:bg-gray-800 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Enter your Email.
-        </h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900 dark:text-white"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 dark:bg-stone-700 dark:text-gray-100 dark:peer-focus:text-white dark:placeholder-white"
-                id="email"
-                type="text"
-                name="email"
-                placeholder="Your email..."
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 dark:text-gray-100 dark:peer-focus:text-white" />
+    <>
+      <form
+        className="space-y-3"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await handleSubmit(e);
+        }}
+      >
+        <div className="flex-1 rounded-lg bg-gray-50 dark:bg-gray-800 px-6 pb-4 pt-8">
+          <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+            Enter your Email.
+          </h1>
+          <div className="w-full">
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900 dark:text-white"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 dark:bg-stone-700 dark:text-gray-100 dark:peer-focus:text-white dark:placeholder-white"
+                  id="email"
+                  type="text"
+                  name="email"
+                  placeholder="Your email..."
+                />
+                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 dark:text-gray-100 dark:peer-focus:text-white" />
+              </div>
             </div>
           </div>
+          <Button
+            className={clsx("mt-4 w-full ", {
+              "mt-4 w-full  cursor-wait loadingLogin disabled ":
+                loading === true,
+            })}
+          >
+            {loading ? "Loading ..." : "Send reset link"}
+            <ArrowRightIcon className="ml-auto h-5 w-5 text-black dark:text-white dark:hover:text-rose-500 hover:text-blue-700 hover:h-7 hover:w-7" />
+          </Button>
+          <div className="flex flex-row justify-center items-center">
+            <InformationCircleIcon className=" text-lg mr-5 pointer-events-none h-[45px] w-[45px] dark:text-blue-300 dark:peer-focus:text-white text-blue-700" />
+            <p className="mt-4 text-xs text-gray-500 dark:text-gray-300">
+              If an account with the email provided exists you will receive an
+              email with a link where you can reset your password!
+            </p>
+          </div>
         </div>
-        <Button
-          className={clsx("mt-4 w-full ", {
-            "mt-4 w-full  cursor-wait loadingLogin disabled ": loading === true,
-          })}
-        >
-          {loading ? "Loading ..." : "Send reset link"}
-          <ArrowRightIcon className="ml-auto h-5 w-5 text-black dark:text-white dark:hover:text-rose-500 hover:text-blue-700 hover:h-7 hover:w-7" />
-        </Button>
-        <div className="flex flex-row justify-center items-center">
-          <InformationCircleIcon className=" text-lg mr-5 pointer-events-none h-[45px] w-[45px] dark:text-blue-300 dark:peer-focus:text-white text-blue-700" />
-          <p className="mt-4 text-xs text-gray-500 dark:text-gray-300">
-            If an account with the email provided exists you will receive an
-            email with a link where you can reset your password!
-          </p>
+      </form>
+      {loading && (
+        <div className="fixed left-0 top-0 bg-[#250e0e49] dark:bg-[#06093f77] w-[100vw] h-[100vh] ">
+          <div className="bg-[#b76973ab] dark:bg-[#113a27e3] dark:text-yellow-300 md:text-lg md:h-16 h-12 w-[30%] rounded-lg flex flex-row justify-center items-center fixed left-[35%] top-1/2">
+            <div className="inline-block md:h-8 md:w-8 h-5 w-5 animate-spin-slow rounded-full mr-4 md:border-4 border-2 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_3s_linear_infinite] dark:text-yellow-300"></div>
+            Please wait...
+          </div>
         </div>
-      </div>
-    </form>
+      )}
+    </>
   );
 }
