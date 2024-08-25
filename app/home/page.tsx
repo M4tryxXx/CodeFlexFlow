252e365cb494472051143c1a287ca2d0fae80abe
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-25 11:03:42 +0100

    update

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -9,8 +9,6 @@ export default async function HomePage() {
   if (user) {
     role = user.role;
   }
-  let country: any;
-  console.log(user);
   if (user && user.lastLoginFrom) {
     //const user = await findUserById(userIdd);
     // const userIp = await fetch("https://api.ipify.org?format=json").then((res) =>
@@ -84,4 +82,35 @@ export default async function HomePage() {
       </main>
     );
   }
+  return (
+    <main className={`${myStyles.main}`}>
+      <div className="flex h-20 shrink-0 items-center justify-between rounded-lg   p-2 md:h[30px]">
+        <AcmeLogo />
+      </div>
+      <div className="mt-4 flex grow  flex-col md:flex-row gap-4 ">
+        <div className="mt-4 justify-around flex flex-row md:flex-col gap-4 w-3/4 h-auto mx-auto md:w-1/5 md:justify-start">
+          <Link
+            href="/home/dashboard"
+            className="flex h-[48px] grow items-center gap-2 rounded-md bg-rose-100 dark:bg-emerald-700 p-3 text-sm font-medium hover:bg-rose-200 hover:text-rose-900 md:flex-none justify-between md:p-2 md:px-3 max-w-40 w-36 dark:text-gray-100 dark:hover:bg-emerald-800"
+          >
+            <span>Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+
+          {role === "admin" ? (
+            <Link
+              href="/home/admin"
+              className="flex h-[48px] grow items-center gap-2 rounded-md bg-rose-100 dark:bg-emerald-700 p-3 text-sm font-medium hover:bg-rose-200 hover:text-rose-900 md:flex-none justify-between md:p-2 md:px-3 max-w-40 w-36 dark:text-gray-100 dark:hover:bg-emerald-800"
+            >
+              <span>Admin</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+          ) : null}
+        </div>
+        <div className="flex items-start justify-center p-6 md:w-4/5 md:px-28 md:py-12">
+          <h1 className="text-3xl font-bold mb-6 text-gray-800 dark:text-gray-200 opacity-75">
+            Welcome to CodeFlexFlow!
+          </h1>
+        </div>
+      </div>
+    </main>
+  );
 }
