55916ee11692d4de89bbe5edf5315e5c1a8975c4
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-25 08:00:21 +0100

    update

--- app/cv/[invitationId]/page.tsx
+++ app/cv/[invitationId]/page.tsx
@@ -22,15 +22,11 @@ export default async function CvPage({
 
   if (!invite) {
     return (
-      <main className="flex h-full flex-col items-center justify-center">
-        <div className="flex h-20 shrink-0 items-center justify-between rounded-lg   p-2 md:h[30px]">
-          <AcmeLogo />
-        </div>
-
+      <main className="flex h-[100vh] flex-col items-center justify-start bg-[#050816]">
         <div className="flex items-center justify-center p-6 md:w-4/5 md:px-28 md:py-12">
           <div className="relative my-5 bg-red-200 rounded-md border">
-            <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900 dark:text-gray-100 dark:peer-focus:text-white" />
-            <div className="rounded-md border border-blue-400 py-2 px-5 pl-10 text-sm outline-2 font-medium dark:bg-blue-950 dark:border-sky-500">
+            <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[21px] w-[21px] -translate-y-1/2 text-red-500 peer-focus:text-gray-900 dark:text-gray-100 dark:peer-focus:text-white" />
+            <div className="rounded-md border border-red-400 py-2 px-5 pl-10 text-sm outline-2 font-medium dark:bg-red-950 dark:border-red-500">
               The invitation Code is invalid or expired!
             </div>
           </div>
