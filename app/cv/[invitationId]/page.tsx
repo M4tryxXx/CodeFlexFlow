490506fde196f6bfa9a0178817fb233fc98125f7
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-25 08:09:18 +0100

    update

--- app/cv/[invitationId]/page.tsx
+++ app/cv/[invitationId]/page.tsx
@@ -25,7 +25,7 @@ export default async function CvPage({
       <main className="flex h-[100vh] flex-col items-center justify-start bg-[#050816]">
         <div className="flex items-center justify-center p-6 md:w-4/5 md:px-28 md:py-12">
           <div className="relative my-5 bg-red-200 rounded-md border">
-            <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[21px] w-[21px] -translate-y-1/2 text-red-500 peer-focus:text-gray-900 dark:text-gray-100 dark:peer-focus:text-white" />
+            <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[21px] w-[21px] -translate-y-1/2 text-red-500 dark:text-red-500" />
             <div className="rounded-md border border-red-400 py-2 px-5 pl-10 text-sm outline-2 font-medium dark:bg-red-200 text-black dark:text-black dark:border-red-400">
               The invitation Code is invalid or expired!
             </div>
