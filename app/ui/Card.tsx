6c38b648f9527cb8aba0efab7a7db967ff9882d4
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-26 12:52:23 +0100

    Update Card.tsx

--- app/ui/Card.tsx
+++ app/ui/Card.tsx
@@ -13,15 +13,15 @@ export default function Card({ title, description, delay, index }: any) {
         scale: 1,
         speed: 650,
       }}
-      className="bg-tertiary p-5 rounded-2xl w-[360px] "
+      className="bg-tertiary p-2 rounded-2xl w-[360px] "
     >
       <motion.div
         variants={fadeIn("up", "spring", delay, 0.75)}
         initial="hidden"
         whileInView= "show"
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        className="w-full green-pink-gradient p-[2px] rounded-[20px] shadow-card"
       >
-        <div className="bg-[#151030] rounded-[20px] py-5 px-12 flex justify-evenly items-center flex-col">
+        <div className="bg-[#151030] rounded-[20px] py-2 px-3 flex justify-evenly items-center flex-col">
           <h1 className="text-2xl font-bold">{title}</h1>
           <hr className="my-4 border-[.3mm] border-gray-500 w-full" />
           <p className="text-gray-700 dark:text-gray-300 indent-6">
