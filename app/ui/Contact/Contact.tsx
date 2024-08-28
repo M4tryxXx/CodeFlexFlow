44d1bf7ae7156be7f948027278dde3d216b1b4fb
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-29 00:12:46 +0100

    Update Contact.tsx

--- app/ui/Contact/Contact.tsx
+++ app/ui/Contact/Contact.tsx
@@ -44,10 +44,15 @@ export default function Contact() {
     <>
       <p
         id="contact"
-        onClick={() => setHidden(!hidden)}
+        onClick={() => {
+          setHidden(!hidden)}
+          if(!hidden) {
+            const element = document.getElementById("contact");
+            element.style.color = "red";
+        }
         className="cursor-pointer font-bold text-blue-500 hover:underline underline-offset-4 dark:text-yellow-300"
       >
-        <a href="#bot">Contact me</a>
+        <a href="#bot">{hidden ? "Contact me" : "Close"}</a>
       </p>
       <motion.div
         layout
