b61f02eab6e97550489e27fe146124af458a6e7b
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-28 09:08:26 +0100

    Update page.tsx

--- app/home/dashboard/page.tsx
+++ app/home/dashboard/page.tsx
@@ -4,7 +4,7 @@ import InvitesTable from "@/app/ui/dashboard/InvitesTable";
 import { userData } from "@/app/lib/actions";
 import { list } from "postcss";
 import { Link } from "@nextui-org/react";
-inport { fristToCapital } from "../../lib/utils";
+import { fristToCapital } from "../../lib/utils";
 
 export default async function dashPage() {
   const user = await userData();
