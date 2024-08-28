66cdab89e736d0abdbf7599d31d295ded7ec0ae5
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-29 00:04:19 +0100

    Update layout.tsx

--- app/home/admin/layout.tsx
+++ app/home/admin/layout.tsx
@@ -14,7 +14,7 @@ export default async function AdminLayout({
 
   const actionUser = async () => {
     if (!user || user.role !== "admin") {
-      await redirectUser();
+      await redirectUser("/home");
     }
   };
 
