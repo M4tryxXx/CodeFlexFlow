2c4e5bc9dce6f21002e327b6f50bd9588e687061
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-29 20:41:21 +0100

    update

--- app/ui/profile/MyCv/Contact.tsx
+++ app/ui/profile/MyCv/Contact.tsx
@@ -24,7 +24,7 @@ export default function ContactCard({ user }: any) {
           restDelta: 0.001,
         },
       }}
-      whileDrag={{ scale: 1, transition: { duration: 0.2 } }}
+      whileDrag={{ scale: 1.2, transition: { duration: 0.3 } }}
       drag
       dragConstraints={{
         top: -500,
