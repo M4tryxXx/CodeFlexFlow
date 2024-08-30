ae7d7de4c3bb6b582c8019fffd822c868f7991dd
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-08-30 08:25:07 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,10 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import ThemeSwitch from "./ui/ThemeSwitch";
-import { getUserLocation } from "@/app/lib/client-actions";
 import ExampleCard from "@/app/ui/ExampleCard";
 import Footer from "@/app/ui/Footer/Footer";
 
 export default function Page() {
-  const location = Intl.DateTimeFormat()
-    .resolvedOptions()
-    .timeZone.split("/")[1];
-
   const data = {
     title: "Code Flex Flow",
     description:
@@ -27,7 +22,10 @@ export default function Page() {
     dates: "August 2024",
   };
 
-  if (location === "Bucharest") {
+  if (
+    Intl.DateTimeFormat().resolvedOptions().timeZone.split("/")[1] ===
+    "Bucharest"
+  ) {
     // if user is from Romania
     return (
       <>
@@ -41,13 +39,15 @@ export default function Page() {
                 href="/register"
                 className="flex flex-col h-8 md:h-7 items-center justify-center rounded-md bg-gray-50 p-1 md:p-2 text-md font-medium hover:bg-rose-200 hover:text-rose-900 dark:hover:text-yellow-300 dark:bg-emerald-950 dark:hover:bg-emerald-800 hover:underline hover:underline-offset-[3px]"
               >
-                <span>Register</span> <ArrowRightIcon className="w-5 md:w-6" />
+                <span>Creați cont</span>{" "}
+                <ArrowRightIcon className="w-5 md:w-6" />
               </Link>
               <Link
                 href="/login"
                 className="flex flex-col h-8 md:h-7 items-center justify-center rounded-md bg-gray-50 p-1 md:p-2 text-md font-medium hover:bg-rose-200 hover:text-rose-900 dark:hover:text-yellow-300 dark:bg-emerald-950 dark:hover:bg-emerald-800 hover:underline hover:underline-offset-[3px]"
               >
-                <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+                <span>Conectați-vă</span>{" "}
+                <ArrowRightIcon className="w-5 md:w-6" />
               </Link>
 
               <div className="flex w-25 items-center gap-5 self-start justify-between rounded-lg bg-rose-500 text-sm font-medium text-white transition-colors hover:bg-rose-400 md:text-base dark:bg-emerald-900 dark:hover:bg-emerald-700">
@@ -57,22 +57,23 @@ export default function Page() {
 
             <div className="flex flex-col gap-4 p-4">
               <h1 className="text-3xl font-bold mb-6 md:mx-10 text-gray-800 dark:text-gray-200 opacity-75">
-                Welcome to CodeFlexFlow!
+                Bine ați venit!
               </h1>
               <div className=" flex flex-col md:flex-row justify-between">
                 <div className="md:w-[50%] p-4">
                   <p className="indent-7">
-                    <span className="text-4xl ">W</span>elcome to CodeFlexFlow!
-                    Our platform empowers you to create and send digital
-                    Curriculum Vitae (CV) effortlessly. Track your CV to see
-                    when and if it has been read, along with the exact date and
-                    time. Enhance your job application process with real-time
-                    insights and a streamlined digital experience. Join us and
-                    take control of your career journey today!
+                    <span className="text-4xl ">B</span>ine ai venit la
+                    CodeFlexFlow! Platforma noastră îți permite să creezi și să
+                    trimiți CV-uri digitale cu ușurință. Urmărește CV-ul tău
+                    pentru a vedea când și dacă a fost citit, împreună cu data
+                    și ora exactă. Îmbunătățește procesul de aplicare pentru
+                    joburi cu informații în timp real și o experiență digitală
+                    simplificată. Alătură-te nouă și preia controlul asupra
+                    carierei tale chiar astăzi!
                   </p>
                 </div>
                 <div className="flex-col md:flex-row p-4  justify-center items-center flex flex-wrap gap-3">
-                  <ExampleCard data={data} delay={0.5} />
+                  <ExampleCard data={dataRomania} delay={0.5} />
                 </div>
               </div>
             </div>
