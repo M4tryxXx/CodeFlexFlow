c3fc46d1b3c94546116213ad2e9b4b0850a0d14f
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-09-03 09:00:58 +0100

    Update create-experience-form.tsx

--- app/ui/experience/create-experience-form.tsx
+++ app/ui/experience/create-experience-form.tsx
@@ -220,7 +220,7 @@ export default function AddExperienceForm({ id }: { id: string }) {
         </div>
         <div className="mt-6 flex justify-end gap-4">
           <Link
-            href="/dashboard/experience"
+            href="/home/dashboard/experiences"
             className="flex h-9 items-center rounded-lg bg-rose-500 dark:bg-emerald-900 dark:hover:bg-emerald-700 px-4 text-sm font-medium text-white transition-colors hover:bg-rose-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-500 active:bg-rose-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50 shadow-md shadow-black"
           >
             Cancel
