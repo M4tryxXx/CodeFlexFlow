00f273e6ff9817415d7cd8831c6ecb662c74c015
Eugen <146987100+M4tryxXx@users.noreply.github.com>
2024-10-31 14:12:17 +0000

    update

--- app/home/dashboard/profile/[profileId]/page.tsx
+++ app/home/dashboard/profile/[profileId]/page.tsx
@@ -2,11 +2,9 @@ import EditProfileForm from "@/app/ui/Home/Profile/MyCv/EditProfileForm";
 import Breadcrumbs from "@/app/ui/Home/Experience/Breadcrumbs";
 import { selectUserFull } from "@/app/lib/myDb";
 
-export default async function EditProfilePage(
-  props: {
-    params: Promise<{ profileId: string }>;
-  }
-) {
+export default async function EditProfilePage(props: {
+  params: Promise<{ profileId: string }>;
+}) {
   const params = await props.params;
   const { profileId } = params;
 
@@ -14,20 +12,7 @@ export default async function EditProfilePage(
 
   return (
     <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          { label: "Dashboard", href: "/home/dashboard" },
-          {
-            label: "Profile",
-            href: "/home/dashboard/profile",
-          },
-          {
-            label: "Edit Profile",
-            href: ``,
-            active: true,
-          },
-        ]}
-      />
+      <Breadcrumbs />
 
       <div className="rounded-md bg-gray-50 p-4 md:p-6 dark:bg-gray-800 my-5">
         <EditProfileForm user={user} />
